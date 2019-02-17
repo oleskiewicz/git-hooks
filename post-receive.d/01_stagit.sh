c6d52dd67eb823a7da0bbff2f6d26f99c498abc4
Piotr Oleskiewicz <piotr.oleskiewicz@mailbox.org>
2019-02-17 23:33:53 +0000

    remove unnecessary "&&"

--- post-receive.d/01_stagit.sh
+++ post-receive.d/01_stagit.sh
@@ -6,8 +6,9 @@ src="$(pwd)"
 dst="/var/www/src.oleskiewi.cz"
 repo="$(basename $src '.git')"
 
-mkdir -p "$dst/$repo"                && \
-cd "$dst/$repo"                      && \
-ln -sf "$dst/style.css" "$dst/$repo" && \
-stagit "$src"                        && \
+mkdir -p "$dst/$repo"
+cd "$dst/$repo"
+ln -sf "$dst/style.css" "$dst/$repo"
+ln -sf "$dst/logo.png" "$dst/$repo"
+stagit -l 30 "$src"
 stagit-index "$src"/../*.git > "$dst/index.html"
